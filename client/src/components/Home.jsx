ccdedcd5edbc9fdf1389dc0ba12a83a7bb4b92f4
shailesh_334 <shaileshgurle334@gmail.com>
2026-09-08 17:51:37 +0530

    copied code from v1

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -14,8 +14,71 @@ useEffect(()=>{
   
   
   return (
-    <div>
-      Home 
+    <div className="min-h-[80vh] flex justify-center items-center flex-wrap p-6">
+     <div className="w-full max-w-sm rounded-xl border bg-white p-5 shadow-md">
+      
+      <h2 className="text-xl font-semibold text-gray-800">
+        title
+      </h2>
+
+      <p className="mt-2 text-gray-600">
+        description
+      </p>
+
+      <button className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+        buttonText
+      </button>
+
+    </div>
+
+     <div className="w-full max-w-sm rounded-xl border bg-white p-5 shadow-md">
+      
+      <h2 className="text-xl font-semibold text-gray-800">
+        title
+      </h2>
+
+      <p className="mt-2 text-gray-600">
+        description
+      </p>
+
+      <button className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+        buttonText
+      </button>
+
+    </div>
+
+     <div className="w-full max-w-sm rounded-xl border bg-white p-5 shadow-md">
+      
+      <h2 className="text-xl font-semibold text-gray-800">
+        title
+      </h2>
+
+      <p className="mt-2 text-gray-600">
+        description
+      </p>
+
+      <button className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+        buttonText
+      </button>
+
+    </div>
+
+
+     <div className="w-full max-w-sm rounded-xl border bg-white p-5 shadow-md">
+      
+      <h2 className="text-xl font-semibold text-gray-800">
+        title
+      </h2>
+
+      <p className="mt-2 text-gray-600">
+        description
+      </p>
+
+      <button className="mt-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+        buttonText
+      </button>
+
+    </div>
     </div>
   )
 }
